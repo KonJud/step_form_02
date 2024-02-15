2b416c4e6fb7fb3474b949784e423f691082e99e
KonJud <deveuudes@gmail.com>
2024-02-15 13:48:03 +0100

    initial commit

---
+++ components/MultiStepForm/StepForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StepForm() {
+  return (
+    <>
+        <div>
+            <h1>Form Here</h1>
+        </div>
+    </>
+  )
+}
---
+++ components/MultiStepForm/Steps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Steps() {
+  return (
+    <>
+        <div>
+            <h2>Steps</h2>
+        </div>
+    </>
+  )
+}
